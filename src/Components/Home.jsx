0ac25a8bb07ffa73240b8883d5bd5a4b8897050c
Rohini talwade <rohinitalwade15@gmail.com>
2025-06-24 23:01:07 +0530

    added support to edited task

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,6 +4,9 @@ const Home = () => {
   const [task, setTask] = useState("");
   const [taskList, setTaskList] = useState([]);
   const [error, setError] = useState("");
+  const [upadteError, setUpadteError] = useState("");
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editingTask, setEditingTask] = useState(null);
 
   function saveTask(e) {
     e.preventDefault();
@@ -17,6 +20,32 @@ const Home = () => {
       setTask("");
     }
   }
+
+  function editTask(index) {
+    setEditingIndex(index);
+    setEditingTask(taskList[index]);
+  }
+
+  function updateTask() {
+    const updatedTask = editingTask.trim();
+    if (updatedTask == "") {
+      setUpadteError("Task cannot be updated as empty!");
+      return;
+    }
+
+    const dupIndex = taskList.findIndex((t) => t == editingTask);
+    if (dupIndex > -1 && dupIndex !== editingIndex) {
+      setUpadteError("Task already exists!");
+      return;
+    }
+    const tasks = taskList.slice();
+    tasks[editingIndex] = editingTask;
+    setTaskList([...tasks]);
+
+    setEditingIndex(null);
+    setEditingTask(null);
+  }
+
   return (
     <div>
       <h1>Welcome to react-todo</h1>
@@ -37,7 +66,27 @@ const Home = () => {
 
       <ul>
         {taskList.map((task, i) => (
-          <li key={i}>{task}</li>
+          <li key={i}>
+            {editingIndex !== i ? (
+              <span>
+                <span>{task}</span>
+                <button onClick={() => editTask(i)}>Edit</button>
+              </span>
+            ) : (
+              <span>
+                <input
+                  type="text"
+                  value={editingTask}
+                  onChange={(e) => {
+                    setEditingTask(e.target.value);
+                    setUpadteError(null);
+                  }}
+                />
+                <button onClick={updateTask}>update</button>
+                <div>{upadteError}</div>
+              </span>
+            )}
+          </li>
         ))}
       </ul>
     </div>
