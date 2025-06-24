6b687947220297e592b265eb74f692a34134857c
Rohini talwade <rohinitalwade15@gmail.com>
2025-06-24 22:14:57 +0530

    added task form

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  function saveTask(e) {
+    e.preventDefault();
+    setTaskList((curVal) => [...curVal, task]);
+    console.log(taskList);
+  }
   return (
     <div>
       <h1>Welcome to react-todo</h1>
+      <form action="" onSubmit={saveTask}>
+        <input
+          type="text"
+          placeholder="enter task"
+          name="task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 };
